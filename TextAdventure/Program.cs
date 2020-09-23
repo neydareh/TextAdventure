63477aad13d417e41a9d2e0e973716396aac0b4f
Olakunle E Neye <emmanuelneye@gmail.com>
2020-09-23 19:59:55 -0400

    Added a Greeting() method to welcome new players to play the game

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -10,11 +10,31 @@ namespace TextAdventure
         }
         public static void StartGame()
         {
-
+            int choice = 0;
+            string playerName = Greeting();
+            Option(playerName);
+            Travel(choice);
         }
-        public static void Option()
+
+        private static string Greeting()
         {
+            Console.WriteLine("Hey there! Welcome to Travel Text Adventure!!");
+            Console.Write("What's your name: ");
+            string playerName = Console.ReadLine();
 
+            return playerName;
         }
+
+        private static void Travel(int choice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Option(string playerName)
+        {
+            Console.WriteLine($"Hello {playerName}");
+            Console.WriteLine("");
+        }
+
     }
 }
