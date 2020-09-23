c47ccc62d67a9fed4152c7107866a6c1c8904362
Olakunle E Neye <emmanuelneye@gmail.com>
2020-09-23 19:45:26 -0400

    Initial Commit

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void StartGame()
+        {
+
+        }
+        public static void Option()
+        {
+
+        }
+    }
+}
